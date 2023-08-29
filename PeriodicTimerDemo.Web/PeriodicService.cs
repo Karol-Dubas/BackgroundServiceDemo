24a70c87c4375ab27e67c2564e9506ec5801dc02
Karol Dubas <krizz.kontakt@gmail.com>
2023-08-29 21:14:01 +0200

    Update background startup delay

--- PeriodicTimerDemo.Web/PeriodicService.cs
+++ PeriodicTimerDemo.Web/PeriodicService.cs
@@ -12,11 +12,18 @@ public class PeriodicService : BackgroundService
     
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        // We still have to wait for this task to finish.
+        // To start this in the background:
+        // - Task.Run
+        // - start services concurrently (configure startup)
+        // - IHostedLifecycleService
+        _logger.LogInformation("Init");
+        await Task.Delay(3000);
+
         while (await _timer.WaitForNextTickAsync(stoppingToken) 
                && !stoppingToken.IsCancellationRequested)
         {
             // Tries to stay as close to the set delay as possible
-            
             _logger.LogInformation(DateTimeOffset.UtcNow.ToString("O"));
         }
     }
