06d9eb6185fcf5f2fa0ed854ce9cd67dd816d0ee
Karol Dubas <krizz.kontakt@gmail.com>
2024-04-27 19:55:42 +0200

    Version update

--- PeriodicTimerDemo.Web/PeriodicService.cs
+++ PeriodicTimerDemo.Web/PeriodicService.cs
@@ -10,18 +10,18 @@ public class PeriodicService : BackgroundService
         _logger = logger;
     }
     
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken ct)
     {
         // We still have to wait for this task to finish.
-        // To start this in the background:
+        // To start this in the background use:
         // - Task.Run
         // - start services concurrently (configure startup)
         // - IHostedLifecycleService
         _logger.LogInformation("Init");
         await Task.Delay(3000);
 
-        while (await _timer.WaitForNextTickAsync(stoppingToken) 
-               && !stoppingToken.IsCancellationRequested)
+        while (await _timer.WaitForNextTickAsync(ct) 
+               && !ct.IsCancellationRequested)
         {
             // Tries to stay as close to the set delay as possible
             _logger.LogInformation(DateTimeOffset.UtcNow.ToString("O"));
